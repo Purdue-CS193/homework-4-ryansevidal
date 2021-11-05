e433ad5010aa451431357b53285ab66ae62064e6
ryansevidal <89810484+ryansevidal@users.noreply.github.com>
2021-11-05 15:10:56 -0400

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -6,27 +6,29 @@ enum Operation
 }
 public class Test1 {
 
-    public static double basicCalculator(final Operation operation, final int num1, final int num2){
+    public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
-	if (operation == Operation.ADD) {
-		result = num1 + num2;
-		
-	} else if (operation == Operation.SUBTRACT) {
-		result = num1 - num2;
-	} else if (operation == Operation.MULTIPLY) {
-		result = num1 * num2;
-	} else if (operation == Operation.DIVIDE) {
-		if (num2 == 0) {
-			System.out.println("Can't divide by 0");
-		} else {
-			result = ((double)num1) / num2;
-		}
+        switch(operation) {
+            case ADD:
+                System.out.println("CS193 sucks");
+                break;
+            case SUBTRACT:
+                System.out.println("CS193 is not everyone's favorite class");
+                break;
+            case MULTIPLY:
+                System.out.println("The CS193 lecturers do not want us to thrive in CS");
+                break;
+            case DIVIDE:
+                if (num2 == 0) {
+                    System.out.println("Error: u suck");
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+        }
 
-	}
-	
-	return result;
-
+        return result;
     }
 
     public static void main(final String[] args) {
